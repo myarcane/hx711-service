08cdba4f72fd8a1afeb65c4f758dcfd6bc62ffce
Florian Mailliet <florian@mailliet.me>
2023-06-05 17:22:33 +0200

    Recalibrate scale

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main() {
   // create a SimpleHX711 object using GPIO pin 5 as the data pin,
   // GPIO pin 6 as the clock pin, -370 as the reference unit, and
   // -367471 as the offset
-  SimpleHX711 hx(5, 6, 19757, -14487);
+  SimpleHX711 hx(5, 6, -47461, -38023);
 
   // set the scale to output weights in ounces
    hx.setUnit(Mass::Unit::LB);
