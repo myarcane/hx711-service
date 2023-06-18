b7957b7359c24cf68ff32cd388b57a1dc6a6b91c
Florian Mailliet <florian@mailliet.me>
2023-06-18 22:29:30 +0200

    New scale calibration

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@ int main() {
   using std::chrono::milliseconds;
 
   // create a SimpleHX711 object using GPIO pin 5 as the data pin,
-  // GPIO pin 6 as the clock pin, -370 as the reference unit, and
-  // -367471 as the offset
-  SimpleHX711 hx(5, 6, -47461, -38023);
+  // GPIO pin 6 as the clock pin, -44101 as the reference unit, and
+  // -36730 as the offset
+  SimpleHX711 hx(5, 6, -44101, -36730);
 
   // set the scale to output weights in ounces
    hx.setUnit(Mass::Unit::LB);
